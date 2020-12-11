ac29339bd4c840c71d3e63f0ca1428e1cab94fd8
ZenzoX <42100836+ZenzoX@users.noreply.github.com>
2020-12-11 11:10:21 +0100

    Added basic connection tests

---
+++ private/config/data/receive/index.php
@@ -0,0 +1,17 @@
+<?php
+include($_SERVER['DOCUMENT_ROOT'].'/VideoBox-app/private/config/connection/index.php');
+
+
+$sql = "SELECT id, username, permission FROM users";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["username"]. " " . $row["permission"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
