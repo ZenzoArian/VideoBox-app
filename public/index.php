3d49ffe864b5246400719688ca67041f1e98fd5d
zenzo <zenzo.arian@xs4all.nl>
2021-01-08 10:56:10 +0100

    Added basic frontend system

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php include '../src/index.php' ?>
+<html>
+    <head></head>
+    <body>
+        <?php include '../src/components/header.php' ?>
+        if pageselector = login include src/pages/login.php
+        
+    </body>
+</html>
