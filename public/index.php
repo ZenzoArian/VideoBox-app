986cc8cb484ded8de988f814fadc0f5ce1fa4832
zenzo <zenzo.arian@xs4all.nl>
2021-01-15 11:04:24 +0100

    Added basic header

--- public/index.php
+++ public/index.php
@@ -1,9 +1,22 @@
-<?php include '../src/index.php' ?>
+<!DOCTYPE HTML>
 <html>
-    <head></head>
+    <head>
+        <title>Video Box</title>
+    </head>
     <body>
         <?php include '../src/components/header.php' ?>
-        if pageselector = login include src/pages/login.php
-        
     </body>
 </html>
+
+<!-- 
+    --- PAGES ---
+    home                  - searchbar, top 5 watched videos overview.
+    login                 - link to create account, link to password forgot.
+    create account        - link to login.
+    password forgot       - link to login, link to create account.
+    video upload          - forum to upload video with content.
+    profile               - overview of uploaded content, create content, update content, delete content.
+    logout                - message when logged out, link to home page.
+    admin users overview  - overview of users, create users, update users, delete users.
+    admin videos overview - overview of content, be able to sort content on users or subject, create content, update content, delete content.
+ -->
---
+++ src/components/header.php
@@ -0,0 +1,12 @@
+<?php
+?>
+<link rel=stylesheet href="../src/themes/style.css" >
+<link rel=stylesheet href="../src/themes/header.css" >
+
+<div class="header--container-outer">
+    <div class="header--container-inner">
+        <div class="header--nav-container">
+            <a href="index.php"><div class="header--nav-item">Home</div></a>
+        </div>
+    </div>
+</div>
