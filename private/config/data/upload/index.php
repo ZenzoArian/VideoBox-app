f887d1d383282ad9ac1e91636321e0cbc3874bac
JanHenst <42889823+JanHenst@users.noreply.github.com>
2021-01-15 16:02:28 +0100

    upload function update

--- private/config/data/upload/index.php
+++ private/config/data/upload/index.php
@@ -10,31 +10,12 @@
 <body>
 
 <?php
-//include($_SERVER['DOCUMENT_ROOT'].'/VideoBox-app/private/config/connection/index.php');
-//
-//
-//// Escape user inputs for security
-//$username = $conn->real_escape_string($_REQUEST['username']);
-//$permission = $conn->real_escape_string($_REQUEST['permission']);
-//
-////The mysqli_real_escape_string() function escapes special characters
-//// in a string and create a legal SQL string to provide security against SQL injection.
-//
-//
-//// Attempt insert query execution
-//$sql = "INSERT INTO users (username, permission) VALUES ('$username', '$permission')";
-//if($conn->query($sql) === true){
-//    echo "Records inserted successfully.";
-//} else{
-//    echo "ERROR: Could not able to execute $sql. " . $conn->error;
-//}
-//
-//// Close connection
-//$conn->close();
-
 // define variables and set to empty values
 $usernameErr = $emailErr = $passwordErr = "";
 $username = $email = $password = "";
+$formCheck1 = false;
+$formCheck2 = false;
+$formCheck3 = false;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
@@ -45,6 +26,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // check if name only contains letters and whitespace
         if (!preg_match("/^[a-zA-Z-' ]*$/",$username)) {
             $usernameErr = "Only letters and white space allowed";
+        } else {
+            $formCheck1 = true;
         }
     }
 
@@ -55,6 +38,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // check if e-mail address is well-formed
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             $emailErr = "Invalid email format";
+        } else {
+            $formCheck2 = true;
         }
     }
 
@@ -62,6 +47,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $passwordErr = "Password is required";
     } else {
         $password = test_input($_POST["password"]);
+        $formCheck3 = true;
     }
 }
 
@@ -71,6 +57,31 @@ function test_input($data) {
     $data = htmlspecialchars($data);
     return $data;
 }
+
+if ($formCheck1 && $formCheck2 && $formCheck3) {
+    include($_SERVER['DOCUMENT_ROOT'].'/VideoBox-app/private/config/connection/index.php');
+
+// Escape user inputs for security
+    $username = $conn->real_escape_string($_REQUEST['username']);
+    $password = $conn->real_escape_string($_REQUEST['password']);
+    $email = $conn->real_escape_string($_REQUEST['email']);
+
+//The mysqli_real_escape_string() function escapes special characters
+// in a string and create a legal SQL string to provide security against SQL injection.
+
+
+// Attempt insert query execution
+    $sql = "INSERT INTO users (username, password, email, permission, accredited) VALUES ('$username', '$password', '$email', 'reporter', false)";
+    if($conn->query($sql) === true){
+        echo "Records inserted successfully.";
+    } else{
+        echo "ERROR: Could not able to execute $sql. " . $conn->error;
+    }
+
+// Close connection
+    $conn->close();
+}
+
 ?>
 
 <p><span class="error">* required field</span></p>
@@ -96,14 +107,10 @@ function test_input($data) {
     <input type="submit" value="Submit">
 </form>
 
-<?php
-echo "<h2>Your Input:</h2>";
-echo $username;
-echo "<br>";
-echo $email;
-echo "<br>";
-echo $password;
-?>
-
+<script>
+    if ( window.history.replaceState ) {
+        window.history.replaceState( null, null, window.location.href );
+    }
+</script>
 </body>
 </html>
