931016445e5802c0ddcb493e69fb9a2fab77b729
Zenzo <zenzo.arian@xs4all.nl>
2021-01-15 16:00:24 +0100

    Created basic files.

---
+++ public/usersOverview.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Video Box</title>
+    </head>
+    <body>
+        <?php include '../src/components/header.php' ?>
+    </body>
+</html>
+
+<!-- 
+    --- PAGES ---
+    home                  - searchbar, top 5 watched videos overview.
+    login                 - link to create account, link to password forgot.
+    create account        - link to login.
+    password forgot       - link to login, link to create account.
+    video upload          - forum to upload video with content.
+    profile               - overview of uploaded content, create content, update content, delete content.
+    logout                - message when logged out, link to home page.
+    admin users overview  - overview of users, create users, update users, delete users.
+    admin videos overview - overview of content, be able to sort content on users or subject, create content, update content, delete content.
+ -->
