adafdac378dd3f6a65b40e9ba76b0786bd77a27f
Zenzo <zenzo.arian@xs4all.nl>
2021-01-18 18:34:08 +0100

    Added first version of profile

--- public/profile.php
+++ public/profile.php
@@ -7,6 +7,49 @@
     <body>
         <?php include '../src/components/header.php' ?>
         <?php include($_SERVER['DOCUMENT_ROOT'].'/VideoBox-app/src/actions/checkLoginUSer.php'); ?>
+        <h2>Video overview and edit/delete</h2>
+
+        <table>
+            <tr>
+                <th>Video:</th>
+                <th>Id:</th>
+                <th>Username:</th>
+                <th>Title:</th>
+                <th>Description:</th>
+                <th>Subject:</th>
+                <th>Views:</th>
+                <th>Edit:</th>
+                <th>Delete:</th>
+            </tr>
+
+            <?php
+
+            include($_SERVER['DOCUMENT_ROOT'].'/VideoBox-app/private/config/connection/index.php');
+
+            $records = mysqli_query($conn,"SELECT id, username, title, description, subject, views, file FROM uploads WHERE username LIKE '{$_SESSION['username']}' ORDER BY id DESC;"); // fetch data from database
+
+            while($data = mysqli_fetch_array($records))
+            {
+                ?>
+                <tr>
+                    <td>
+                        <video width="200" height="100" controls>
+                            <source src="../src/uploads/<?php echo $data['file']; ?>" type="video/mp4">
+                        </video>
+                    </td>
+                    <td><?php echo $data['id']; ?></td>
+                    <td><?php echo $data['username']; ?></td>
+                    <td><?php echo $data['title']; ?></td>
+                    <td><?php echo $data['description']; ?></td>
+                    <td><?php echo $data['subject']; ?></td>
+                    <td><?php echo $data['views']; ?></td>
+                    <td><a href="../private/config/data/edit/editVideo.php?id=<?php echo $data['id']; ?>">Edit</a></td>
+                    <td><a href="../private/config/data/edit/deleteVideo.php?id=<?php echo $data['id']; ?>">Delete</a></td>
+                </tr>
+                <?php
+            }
+            ?>
+        </table>
     </body>
 </html>
 
