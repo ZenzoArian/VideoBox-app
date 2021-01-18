49179af60dfc53af463242ffa36b322cd84ea85f
Zenzo <zenzo.arian@xs4all.nl>
2021-01-18 15:31:56 +0100

    First iteration of password forgot

--- public/forgotPassword.php
+++ public/forgotPassword.php
@@ -1,3 +1,30 @@
+<?php
+include($_SERVER['DOCUMENT_ROOT'].'/VideoBox-app/private/config/connection/index.php');
+if(isset($_POST['submit']))
+{
+    $user_id = $_POST['user_id'];
+    $result = mysqli_query($conn,"SELECT * FROM user_details where user_id='" . $_POST['user_id'] . "'");
+    $sql = "SELECT * FROM users where username='" . $_POST['user_id'] . "'";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+	$fetch_user_id=$row['username'];
+	$email_id=$row['email'];
+	$password=$row['password'];
+	    if($user_id==$fetch_user_id) {
+            $to = $email_id;
+            $subject = "Password";
+            $txt = "Your password is : $password.";
+            $headers = "From: zenzo.arian@xs4all.nl" . "\r\n" .
+            "CC: zenzo.arian@xs4all.nl";
+            mail($to,$subject,$txt,$headers);
+        }
+            else{
+                echo 'invalid userid';
+            }
+}
+?>
+
+
 <!DOCTYPE HTML>
 <html>
     <head>
@@ -5,6 +32,11 @@
     </head>
     <body>
         <?php include '../src/components/header.php' ?>
+        <form action='' method='post'>
+        <table cellspacing='5'>
+        <tr><td>user id:</td><td><input type='text' name='user_id'/></td></tr>
+        <tr><td></td><td><input type='submit' name='submit' value='Submit'/></td></tr>
+        </table>
     </body>
 </html>
 
