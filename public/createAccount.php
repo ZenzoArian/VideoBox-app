e3635cd1f1c4609b2aa8d3360c6574ba781d9d81
Zenzo <zenzo.arian@xs4all.nl>
2021-01-21 20:46:27 +0100

    Fine-tuned video upload.

--- public/createAccount.php
+++ public/createAccount.php
@@ -13,24 +13,24 @@
 <p><span class="error">* required field</span></p>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
     <p>
-        <label for="username">Username:</label>
-        <input type="text" name="username" id="username" maxlength="15">
+        <label for="username">Username:</label><br>
+        <input type="text" name="username" id="username" maxlength="15" class="input--field input-small">
         <span class="error">* <?php echo $usernameErr;?></span>
         <br><br>
     </p>
     <p>
-        <label for="email">Email:</label>
-        <input type="text" name="email" id="email" maxlength="50">
+        <label for="email">Email:</label><br>
+        <input type="text" name="email" id="email" maxlength="50" class="input--field input-small">
         <span class="error">* <?php echo $emailErr;?></span>
         <br><br>
     </p>
     <p>
-        <label for="password">Password:</label>
-        <input type="password" name="password" id="password" maxlength="20">
+        <label for="password">Password:</label><br>
+        <input type="password" name="password" id="password" maxlength="20" class="input--field input-small">
         <span class="error">* <?php echo $passwordErr;?></span>
         <br><br>
     </p>
-    <input type="submit" value="Submit">
+    <input type="submit" value="Submit" class="form--submit-button">
 </form>
 
 <script>
--- public/login.php
+++ public/login.php
@@ -9,22 +9,22 @@
     <body>
         <?php include '../src/components/header.php' ?>
         
-        <a href="forgotPassword.php">   <div class="">forgotPassword</div></a>
         <p><span class="error">* required field</span></p>
         <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
             <p>
-                <label for="username">Username:</label>
-                <input type="text" name="username" id="username" maxlength="15">
+                <label for="username">Username:</label><br>
+                <input type="text" name="username" id="username" maxlength="15" class="input--field input-small">
                 <span class="error">* <?php echo $usernameErr;?></span>
                 <br><br>
             </p>
             <p>
-                <label for="password">Password:</label>
-                <input type="password" name="password" id="password" maxlength="20">
+                <label for="password">Password:</label><br>
+                <input type="password" name="password" id="password" maxlength="20" class="input--field input-small">
                 <span class="error">* <?php echo $passwordErr;?></span>
                 <br><br>
             </p>
-            <input type="submit" value="Submit">
+            <a href="">   <div class="">forgotPassword</div></a>
+            <input type="submit" value="Submit" class="form--submit-button">
         </form>
         <script>
             if ( window.history.replaceState ) {
